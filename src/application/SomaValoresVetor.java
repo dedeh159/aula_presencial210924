ef8098187006cf0b5a1959dfc2c60f2146fa24d6
Andre Fernandes <dedeh159@gmail.com>
2024-09-21 11:11:21 -0300

    atualizando

--- src/application/SomaValoresVetor.java
+++ src/application/SomaValoresVetor.java
@@ -1,4 +1,27 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class SomaValoresVetor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> lista = new ArrayList<Integer>();
+        lista.addAll(List.of(59,28,3,65,32));
+
+        System.out.println("Digite o valor de X (0-4): ");
+        int x = sc.nextInt();
+        System.out.println("Digite o valor de Y (0-4): ");
+        int y = sc.nextInt();
+
+        if (x >= 0 && x <= 4 && y >= 0 && y <= 4) {
+            int soma = lista.get(x) + lista.get(y);
+            System.out.println("A soma dos valores em X e Y é " + soma);
+        }
+        else {
+            System.out.println("Posições inválidas, X e Y devem estar entre 0 e 4.");
+        }
+        sc.close();
+    }
 }
--- src/application/TresVetores.java
+++ src/application/TresVetores.java
@@ -1,4 +1,25 @@
 package application;
 
 public class TresVetores {
+    public static void main(String[] args) {
+        int[] vetor1 = new int[5];
+        int[] vetor2 = new int[5];
+        int[] vetor3 = new int[10];
+
+        for (int i = 0; i < vetor1.length; i++) {
+            System.out.println("Preenchendo valores do vetor");
+            vetor1[i] = i * 2;
+            vetor2[i] = i * 2 + 1;
+        }
+        for (int i = 0; i < 5; i++) {
+            vetor3[i * 2] = vetor1[i];
+            vetor3[i * 2 + 1] = vetor2[i];
+        }
+        System.out.println("Vetor resultante: ");
+        for (int valor : vetor3) {
+            System.out.println(valor + " ");
+        }
+
+
+    }
 }
