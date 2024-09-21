a32ccb97061d50da4cf49374c113d27f7f1d22e6
Andre Fernandes <dedeh159@gmail.com>
2024-09-21 13:23:12 -0300

    adicionando envios

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,15 @@
 package application;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for (int i = 2; i <= n; i += 2) {
+            System.out.println(i+"^"+2+" = " + (i*i));
+
+        }
+    }
 }
