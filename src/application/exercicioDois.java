7b1fe6d4f16c0d9fa2d286a894d152916fcc6a12
Andre Fernandes <dedeh159@gmail.com>
2024-09-21 10:22:37 -0300

    atualizado 10:22

---
+++ src/application/exercicioDois.java
@@ -0,0 +1,13 @@
+package application;
+
+public class exercicioDois {
+    public static void main(String[] args) {
+        int x = 1;
+        int soma = 0;
+        while (x < 100) {
+            soma += x;x
+            x += 2;
+        }
+        System.out.println(soma);
+    }
+}
