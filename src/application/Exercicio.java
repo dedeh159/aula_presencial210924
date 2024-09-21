275fd4954c2c783e74478eacd380b85f0c1cb7ab
Andre Fernandes <dedeh159@gmail.com>
2024-09-21 09:40:06 -0300

    teste

--- src/application/Exercicio.java
+++ src/application/Exercicio.java
@@ -1,4 +1,10 @@
 package application;
 
 public class Exercicio {
+    public static void main(String[] args) {
+
+        for (int x = 2; x < 11; x += 2){
+            System.out.println(x);
+        }
+    }
 }
